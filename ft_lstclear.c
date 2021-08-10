e73346dcc64baf47675ae69db4f882d6d56a7c26
Warley Juneo <warleyjuneo111@gmail.com>
2021-08-10 04:06:26 -0300

    %100

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wjuneo-f <wjuneo-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/10 03:43:55 by wjuneo-f          #+#    #+#             */
-/*   Updated: 2021/08/10 03:43:56 by wjuneo-f         ###   ########.fr       */
+/*   Created: 2021/08/10 03:47:24 by wjuneo-f          #+#    #+#             */
+/*   Updated: 2021/08/10 03:48:03 by wjuneo-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	size_t	s_len;
-	size_t	i;
-
-	s_len = ft_strlen(s);
-	i = 0;
-	if (!s)
-		return ;
-	while (i < s_len)
-	{
-		f(i, &s[i]);
-		i++;
-	}
+	
 }
